5c950e8d5d8b2c6e14740c401ddfee660b820247
rdnrn <rodina.arina00@gmail.com>
2023-01-19 21:04:30 +0300

    add solution

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -9,26 +10,31 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class MainApp {
-   public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
+    public static void main(String[] args) throws SQLException {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
 
-      UserService userService = context.getBean(UserService.class);
+        UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+        userService.add(new User("BoJack", "Horseman", "greatdepression@mail.ru", new Car("Омепразол", 233546)));
+        userService.add(new User("Tinky", "Winky", "tinky_winky87@mail.ru", new Car("Супер-пупер 2.0 ЭКО ВЕГАН", 234546)));
+        userService.add(new User("Sponge Bob", "SquarePants", "penpineappleapplepen@mail.ru", new Car("Супер-пупер 2.0 ЭКО ВЕГАН", 235546)));
+        userService.add(new User("Arania", "Gradne", "yah@mail.ru", new Car("Ultra Deluxe Rich", 235786)));
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
-      }
 
-      context.close();
-   }
+        List<User> users = userService.listUsers();
+        for (User user : users) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println("Car Model = " + user.getCar().getModel());
+            System.out.println();
+        }
+
+        User user = userService.getUserByCar("Омепразол", 233546);
+        System.out.println("GET USER BY CAR: " + user.toString());
+
+        context.close();
+    }
 }
--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -10,20 +10,29 @@ import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
+    private static final String GET_USER_BY_CAR = "FROM User user WHERE user.car.model = :model AND user.car.series = :series";
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    public User getUserByCar(String model, int series) {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(GET_USER_BY_CAR);
+        query.setParameter("model", model);
+        query.setParameter("series", series);
+        return query.setMaxResults(1).getSingleResult();
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
 
 }
